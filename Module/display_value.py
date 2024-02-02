d8379ba1145ce425d087b78ff6f3f641f32b023b
AFlo59 <etudessup59230@gmail.com>
2024-02-02 16:34:12 +0100

    up

--- Module/display_value.py
+++ Module/display_value.py
@@ -1,16 +1,18 @@
 def display_unique_categorical_values(df):
     """
     Affiche les valeurs uniques de chaque colonne catégorielle dans un DataFrame,
-    ainsi que leur pourcentage et leur nombre brut.
+    ainsi que leur pourcentage, leur nombre brut et le nombre de valeurs manquantes.
 
     Args:
         df (pandas.DataFrame): Le DataFrame à analyser.
     """
     for column in df.select_dtypes(include=['object', 'category']).columns:
         total_count = df[column].count()
+        missing_count = df[column].isnull().sum()
         unique_values = df[column].value_counts()
         print(f"Colonne '{column}':")
         for value, count in unique_values.items():
             percentage = (count / total_count) * 100
             print(f"- {value}: {count} ({percentage:.2f}%)")
+        print(f"  Valeurs manquantes: {missing_count}")
         print("\n")
